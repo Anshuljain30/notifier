8667aa66ce99106276d230389dabfa967e821851
Anshul Jain <anshul_jain@live.com>
2022-07-09 23:15:56 +0530

    Initial Build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Title from "./components/Title";
+import AddNotification from "./components/AddNotification";
+import { collection, query, getDocs, where } from "firebase/firestore";
+import { db } from "./firebase";
+import Noti from "./components/Noti";
 
 function App() {
+  const [posts, setPosts] = React.useState([]);
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const xxx = collection(db, "notification");
+      const q = query(
+        xxx,
+        where("date", ">", new Date(Date.now() - 7 * 24 * 60 * 60 * 1000)),
+        where("date", "<=", new Date(Date.now() + 24 * 60 * 60 * 1000))
+      );
+      const querySnapshot = await getDocs(q);
+      let todosArray = [];
+      querySnapshot.forEach((doc) => {
+        todosArray.push({ ...doc.data(), id: doc.id });
+      });
+      setPosts(todosArray);
+    }
+    fetchData();
+  }, []);
+  console.log(posts.length);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Title />
+      </div>
+      <div>
+        <AddNotification />
+      </div>
+      {posts.length > 0 && (
+        <div className="todo_container">
+          {posts.map((todo) => (
+            <Noti key={todo.id} todo={todo} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
